3e263405bea0535ebd243503e16ffa19c0cb52ba
Marco Gil Flores <usr3886@salesianos-lacuesta.net>
2024-05-28 23:42:59 +0100

    feat(Features): editRestaurant added
    I implemented editRestaurant that can edit all parameters of a restaurant that alredy exist as the name, the location for example.

--- salesianos/net/features/features.java
+++ salesianos/net/features/features.java
@@ -2,16 +2,69 @@ package features;
 
 import java.util.ArrayList;
 
+import java.util.Scanner;
+
 import restaurants.Restaurants;
 
-public class Features extends Restaurants{
+public class Features extends Restaurants {
+
+    Scanner scanner = new Scanner(System.in);
 
     public Features(ArrayList<Restaurants> restaurants, String name, String location, String schelude, String score) {
         super(restaurants, name, location, schelude, score);
     }
-    
-    public void addRestaurant(String name,String location,String schelude,String score){
-        Restaurants restaurant = new Restaurants(restaurants, name, location, schelude, score);
-       this. restaurants.add(restaurant);
+
+    public void addRestaurant(String name, String location, String schelude, String score) {
+        Restaurants restaurant = new Restaurants(restaurantsObjects, name, location, schelude, score);
+        this.restaurantsObjects.add(restaurant);
+        System.out.println("Restaurant" + restaurant.getName() + "was added successfully");
+    }
+
+    public void editRestaurant(String restaurantName) {
+        for (Restaurants restaurants : restaurantsObjects) {
+            if (restaurants.getName().equals(restaurantName)) {
+                System.out.println("What thing do you want to edit about the restaurant" + restaurants.getName() + "?");
+                System.out.println("""
+                        1.Name
+                        2.Location
+                        3.Schelude
+                        4.Score
+
+                        Choose what registry want you to edit and write the number
+                        """);
+                String option = scanner.nextLine();
+                switch (option) {
+                    case "1":
+                        System.out.println("Wich name want you to put?");
+                        String name = scanner.nextLine();
+                        restaurants.setName(name);
+                        System.out.println("Name of the restaurant was changed to;" + name);
+                        break;
+                    case "2":
+                        System.out.println("Wich location want you to put?");
+                        String location = scanner.nextLine();
+                        restaurants.setLocation(location);
+                        System.out.println("Location name of the restaurant was changed to;" + location);
+                        break;
+                    case "3":
+                        System.out.println("Wich schelude want you to put?");
+                        String schelude = scanner.nextLine();
+                        restaurants.setSchelude(schelude);
+                        System.out.println("The schelude of the restaurant was changed to;" + schelude);
+                        break;
+                    case "4":
+                        System.out.println("Wich name what do you want to put?");
+                        String score = scanner.nextLine();
+                        restaurants.setScore(score);
+                        System.out.println("The score of the restaurant was changed to;" + score);
+                        break;
+                    default:
+                    System.out.println("You dind't choose a correct option.");
+                        break;
+                }
+            }
+
+        }
+        System.out.println("The program couldnt find the restaurant that you wasnt looking for o it doesnt exist");
     }
 }
