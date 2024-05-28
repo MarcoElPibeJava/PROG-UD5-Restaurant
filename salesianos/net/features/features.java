d259b68aed67c892a1eb3c11fe95bfefe7442268
Marco Gil Flores <usr3886@salesianos-lacuesta.net>
2024-05-28 21:20:06 +0100

    feat(Features): AddRestaurant was created
    The method addRestaurant was finished for the user.

--- salesianos/net/features/features.java
+++ salesianos/net/features/features.java
@@ -1,5 +1,17 @@
 package features;
 
-public class Features {
+import java.util.ArrayList;
+
+import restaurants.Restaurants;
+
+public class Features extends Restaurants{
+
+    public Features(ArrayList<Restaurants> restaurants, String name, String location, String schelude, String score) {
+        super(restaurants, name, location, schelude, score);
+    }
     
+    public void addRestaurant(String name,String location,String schelude,String score){
+        Restaurants restaurant = new Restaurants(restaurants, name, location, schelude, score);
+       this. restaurants.add(restaurant);
+    }
 }
