059d7eb4607d4350d26dcc0495633c52f3f35ad3
Marco Gil Flores <usr3886@salesianos-lacuesta.net>
2024-05-28 18:49:47 +0100

    build(restaurants): have all necesary methods now
    Created arraylist to saving all restaurants objects, all atributes for inizialize them in the constructor, getters and setters.

--- salesianos/net/restaurants/restaurants.java
+++ salesianos/net/restaurants/restaurants.java
@@ -1,5 +1,52 @@
 package restaurants;
 
+import java.util.ArrayList;
+
 public class restaurants {
+    public static ArrayList <restaurants> restaurants = new ArrayList<>();
+    private String name;
+    private String location;
+    private String schelude;
+    private String score;
+    
+    public restaurants(String name, String location, String schelude, String score) {
+        this.name = name;
+        this.location = location;
+        this.schelude = schelude;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSchelude() {
+        return schelude;
+    }
+
+    public void setSchelude(String schelude) {
+        this.schelude = schelude;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
     
 }
