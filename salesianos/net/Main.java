9d5f9a486f0a6ef071383dffe78b738559d0fefb
Marco Gil Flores <usr3886@salesianos-lacuesta.net>
2024-06-06 20:33:05 +0100

    refactor(features): Methods were moved to Main
    the methods in class Features where moved to class Main.

--- salesianos/net/Main.java
+++ salesianos/net/Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import features.Features;
 import restaurants.Restaurant;
 
 public class Main {
@@ -15,8 +14,8 @@ public class Main {
             System.out.println("""
                     1.-Add restaurant
                     2.-Edit restaurant
-                    3.-Show restaurants
-                    4.-Delete restaurants
+                    3.-Show restaurant
+                    4.-Delete restaurant
                     Q.Stop program
                     """);
             op = scanner.nextLine().toLowerCase();
@@ -30,19 +29,90 @@ public class Main {
                     String schelude = scanner.nextLine().trim().toLowerCase();
                     System.out.println("and for the end the score");
                     String score = scanner.nextLine().trim().toLowerCase();
-                    Features.addRestaurant(restaurantName, location, schelude, score);
+                    Restaurant restaurant = new Restaurant(restaurantName, location, schelude, score);
+                    restaurantsObjects.add(restaurant);
+                    System.out.println("Restaurant " + restaurant.getName() + " was added successfully");
                     break;
                 case "2":
                     System.out.println("wich restaurant do you want to edit?");
-                    String name = scanner.nextLine();
-                    Features.editRestaurant(name);
+                    String nameEdit = scanner.nextLine();
+                    for (Restaurant restaurants : restaurantsObjects) {
+                        if (restaurants.getName().equals(nameEdit)) {
+                            System.out
+                                    .println("What thing do you want to edit about the restaurant; "
+                                            + restaurants.getName() + "?");
+                            System.out.println("""
+                                    1.Name
+                                    2.Location
+                                    3.Schelude
+                                    4.Score
+
+                                    Choose what registry want you to edit and write the number
+                                    """);
+                            String option = scanner.nextLine();
+                            switch (option) {
+                                case "1":
+                                    System.out.println("Wich name want you to put?");
+                                    String nameEditRestaurant = scanner.nextLine().trim().toLowerCase();
+                                    restaurants.setName(nameEditRestaurant);
+                                    System.out.println("Name of the restaurant was changed to; " + nameEditRestaurant);
+                                    break;
+                                case "2":
+                                    System.out.println("Wich location want you to put?");
+                                    String locationEdit = scanner.nextLine().trim().toLowerCase();
+                                    restaurants.setLocation(locationEdit);
+                                    System.out
+                                            .println("Location name of the restaurant was changed to; " + locationEdit);
+                                    break;
+                                case "3":
+                                    System.out.println("Wich schelude want you to put?");
+                                    String scheludeEdit = scanner.nextLine().trim().toLowerCase();
+                                    restaurants.setSchelude(scheludeEdit);
+                                    System.out
+                                            .println("The schelude of the restaurant was changed to; " + scheludeEdit);
+                                    break;
+                                case "4":
+                                    System.out.println("Wich name what do you want to put?");
+                                    String scoreEdit = scanner.nextLine().trim().toLowerCase();
+                                    restaurants.setScore(scoreEdit);
+                                    System.out.println("The score of the restaurant was changed to; " + scoreEdit);
+                                    break;
+                                default:
+                                    System.out.println("You dind't choose a correct option.");
+                                    break;
+                            }
+                        }
+
+                    }
+                    System.out.println(
+                            "The program couldnt find the restaurant that you was looking for o it doesnt exist");
                     ;
                     break;
                 case "3":
-                    Features.showAllRestaurants();
+                    System.out.println("*****Data of all restaurants*****\n");
+                    for (Restaurant restaurants : restaurantsObjects) {
+                        System.out.println("Name of the restaurant; " + restaurants.getName() +
+                                "\n" + "Name of the location; " + restaurants.getLocation() + "\n"
+                                + "The schelude; " + restaurants.getSchelude() + "\n" +
+                                "The score; " + restaurants.getScore());
+                        System.out.println("*************************************");
+                    }
+                    if (restaurantsObjects.isEmpty()) {
+                        System.out.println("You didnt add a restaurant yet");
+                    }
                     break;
                 case "4":
-                    Features.deleteRestaurants();
+                    System.out.println("Wich restaurant do you want to erase?");
+                    String opDelete = scanner.nextLine().toLowerCase().trim();
+                    for (Restaurant restaurants : restaurantsObjects) {
+                        if (opDelete.equals(restaurants.getName())) {
+                            restaurantsObjects.remove(restaurants);
+                            System.out.println("Restaurant deleted successfully.");
+                        }
+                    }
+                    if (restaurantsObjects.isEmpty()) {
+                        System.out.println("The restaurant that you are looking for it doesnt exists");
+                    }
                     break;
                 case "q":
                     System.out.println("stopping program");
--- salesianos/net/features/features.java
+++ salesianos/net/features/features.java
@@ -12,88 +12,20 @@ public class Features extends Restaurant {
         super(name, location, schelude, score);
     }
 
-    public static void addRestaurant(String name, String location, String schelude, String score) {
-        Restaurant restaurant = new Restaurant(name, location, schelude, score);
-        restaurantsObjects.add(restaurant);
-        System.out.println("Restaurant " + restaurant.getName() + " was added successfully");
+    pub
     }
 
     public static void editRestaurant(String restaurantName) {
-        for (Restaurant restaurants : restaurantsObjects) {
-            if (restaurants.getName().equals(restaurantName)) {
-                System.out
-                        .println("What thing do you want to edit about the restaurant; " + restaurants.getName() + "?");
-                System.out.println("""
-                        1.Name
-                        2.Location
-                        3.Schelude
-                        4.Score
-
-                        Choose what registry want you to edit and write the number
-                        """);
-                String option = scanner.nextLine();
-                switch (option) {
-                    case "1":
-                        System.out.println("Wich name want you to put?");
-                        String name = scanner.nextLine().trim().toLowerCase();
-                        restaurants.setName(name);
-                        System.out.println("Name of the restaurant was changed to; " + name);
-                        break;
-                    case "2":
-                        System.out.println("Wich location want you to put?");
-                        String location = scanner.nextLine().trim().toLowerCase();
-                        restaurants.setLocation(location);
-                        System.out.println("Location name of the restaurant was changed to; " + location);
-                        break;
-                    case "3":
-                        System.out.println("Wich schelude want you to put?");
-                        String schelude = scanner.nextLine().trim().toLowerCase();
-                        restaurants.setSchelude(schelude);
-                        System.out.println("The schelude of the restaurant was changed to; " + schelude);
-                        break;
-                    case "4":
-                        System.out.println("Wich name what do you want to put?");
-                        String score = scanner.nextLine().trim().toLowerCase();
-                        restaurants.setScore(score);
-                        System.out.println("The score of the restaurant was changed to; " + score);
-                        break;
-                    default:
-                        System.out.println("You dind't choose a correct option.");
-                        break;
-                }
-            }
-
-        }
-        System.out.println("The program couldnt find the restaurant that you was looking for o it doesnt exist");
+       
     }
 
     public static void showAllRestaurants() {
-        System.out.println("*****Data of all restaurants*****\n");
-        for (Restaurant restaurants : restaurantsObjects) {
-            System.out.println("Name of the restaurant; " + restaurants.getName() +
-                    "\n" + "Name of the location; " + restaurants.getLocation() + "\n"
-                    + "The schelude; " + restaurants.getSchelude() + "\n" +
-                    "The score; " + restaurants.getScore());
-            System.out.println("*************************************");
-        }
-        if (restaurantsObjects.isEmpty()) {
-            System.out.println("You didnt add a restaurant yet");
-        }
+        
 
     }
 
     public static void deleteRestaurants() {
-        System.out.println("Wich restaurant do you want to erase?");
-        String op = scanner.nextLine().toLowerCase().trim();
-        for (Restaurant restaurants : restaurantsObjects) {
-            if (op.equals(restaurants.getName())) {
-                restaurantsObjects.remove(restaurants);
-                System.out.println("Restaurant deleted successfully.");
-            }
-        }
-        if (restaurantsObjects.isEmpty()) {
-            System.out.println("The restaurant that you are looking for it doesnt exists");
-        }
+        
     }
 
 }
